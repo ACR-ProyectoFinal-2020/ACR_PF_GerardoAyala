a16818c04519aced5361ad7b227c71e42c846172
Gerardo Ayala Juárez <23099174+Sataroto@users.noreply.github.com>
2020-06-11 11:49:08 -0500

    Inicializacion
    XD

---
+++ ProyectoFinal/src/Inicializacion/introduccion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Inicializacion;
+
+/**
+ *
+ * @author futbo
+ */
+public class introduccion {
+    public static void main (String[] aggg){
+        System.out.println("xd");
+    }
+}
